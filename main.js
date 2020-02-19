dd3911eb2cf61703768ad24a7ad6bec2433248b9
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-19 15:23:49 -0500

    finish

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function getFirstItemFrom(array1){
+  return array1.shift()
+}
 
+function getLastItemFrom(array1){
+  return array1.pop()
+}
 
+function getIndex3(array1){
+  if (array1.length > 4)
+  return array1[3]
+  if (array1.length < 4)
+  return array1.pop()
 
+}
+function isLongList(array){
+  if (array.length >= 10) {
+    return true
+  }else {return false}
+  
+}
+function firstItemIsNumber(array){
+  if (typeof array[0] === 'number'){
+    return true
+  } else return false
 
+}
 
-
+function secondCharOfThirdString(array){
+  return array[2][1]
+}
 
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
